95a4ebf5303b124025169aad06951e629886ef3e
xiaoming <1756329434@qq.com>
2019-11-04 22:07:06 +0800

    last

--- db/index.js
+++ db/index.js
@@ -1,10 +1,22 @@
 // 连接数据库
 const mongoose = require('mongoose')
 const { DB_URL } = require('../params')
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  keepAlive: true,
+  keepAliveInitialDelay: 300000,
+  socketTimeoutMS: 30000,
+  poolSize: 50,
+  reconnectTries: Number.MAX_VALUE,
+  reconnectInterval: 500,
+  autoReconnect: true,
+
+}
 module.exports = new Promise((resolve, reject) => {
   // 连接数据库异步代码
-  mongoose.set(`useFindAndModify`,false)
-  mongoose.connect(DB_URL,{ useNewUrlParser: true , useUnifiedTopology: true ,})
+  mongoose.set(`useFindAndModify`, false)
+  mongoose.connect(DB_URL,options)
   mongoose.connection.on('open', err => {
     if (err) {
       return reject(err)
